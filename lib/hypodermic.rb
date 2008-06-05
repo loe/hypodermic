a437257c4767a6c8cda4b74ae9f180884731d45e
W. Andrew Loe III <andrew@andrewloe.com>
2008-06-05 14:27:07 -0700

    First pass.

--- lib/hypodermic.rb
+++ lib/hypodermic.rb
@@ -1,7 +1,33 @@
-require 'rubyzip'
-require 'tempfile'
+require 'rubygems'
+require 'zip/zip'
 require 'hypodermic/version'
 
-Module Hypodermic
+class Hypodermic
 
+  def self.extract(path, opts = {})
+    document = self.document(path)
+    
+    if opts[:thumbnail]
+      thumbnail = self.thumbnail(path)
+    end
+    
+    return document, thumbnail
+  end
+  
+  private
+  
+  def self.document_xml(path)
+    f = Zip::ZipFile.open(path) { |z| z.read('word/document.xml') }
+    f.read
+  end
+  
+  def self.document(path)
+    doc_xml = self.document_xml(path)
+    doc_xml.gsub(/<.*?>/, ' ')
+  end
+  
+  def self.thumbnail(path)
+    Zip::ZipFile.open(path) { |z| z.read('docProps/thumbnail.jpeg') }
+  end
+      
 end
--- spec/hypodermic/hypodermic_spec.rb
+++ spec/hypodermic/hypodermic_spec.rb
@@ -1,5 +1,50 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../helper")
- 
-describe 'Hypodermic' do
 
+module HypodermicSpecHelper
+  def stub_document
+    @path = File.expand_path("#{File.dirname(__FILE__)}/document.xml")
+    @mockument = File.open(@path)
+    Zip::ZipFile.stub!(:open).and_return(@mockument)
+  end
+end
+ 
+describe Hypodermic do
+  include HypodermicSpecHelper
+  
+  describe '.extract' do
+    # it 'should not raise an error' do
+    #   lambda { Hypodermic.extract('path', :thumbnail => true) }.should_not raise_error
+    # end
+  end  
+  
+  describe ".document_xml" do
+    before(:each) do
+      stub_document
+    end
+    
+    it "should not raise an error, given a path argument" do
+      lambda { Hypodermic.send(:document_xml, "path") }.should_not raise_error
+    end
+    
+    it "should return the document.xml" do
+      result = Hypodermic.send(:document_xml, "path")
+      result.should == File.read(@path)
+    end
+  end
+  
+  describe ".document" do
+    before(:each) do
+      stub_document
+    end
+    
+    it "should not raise an error" do
+      lambda { Hypodermic.send(:document, "path") }.should_not raise_error
+    end
+    
+    it "should return the file without tags" do
+      result = Hypodermic.send(:document, "path")
+      result.should == File.read(@path).gsub(/<.*?>/, ' ')
+    end
+  end
+  
 end
